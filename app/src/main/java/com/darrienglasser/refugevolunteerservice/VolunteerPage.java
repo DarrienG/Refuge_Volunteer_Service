538b7305cddec109c9130b413b18157cdc00d7de
Darrien Glasser <darrienglasser@outlook.com>
2016-02-13 00:51:30 -0500

    Implement UI for secondary screen

---
+++ app/src/main/java/com/darrienglasser/refugevolunteerservice/VolunteerPage.java
@@ -0,0 +1,32 @@
+package com.darrienglasser.refugevolunteerservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class VolunteerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_page);
+
+        TextView noReqView = (TextView) findViewById(R.id.no_req);
+        RelativeLayout foundReq = (RelativeLayout) findViewById(R.id.cardLayoutId);
+
+        // TODO: Poll server to see if data has been received
+        if (false) {
+            foundReq.setVisibility(View.GONE);
+        } else {
+            noReqView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Do nothing
+        // We don't want to let the user go back to the parent activity
+    }
+}

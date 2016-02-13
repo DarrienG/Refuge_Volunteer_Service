aa896cedb8216a44c5f4c77af383010d18139527
Darrien Glasser <darrienglasser@outlook.com>
2016-02-13 05:32:41 -0500

    Change refresh service, bind text to views

--- app/src/main/java/com/darrienglasser/refugevolunteerservice/VolunteerPage.java
+++ app/src/main/java/com/darrienglasser/refugevolunteerservice/VolunteerPage.java
@@ -1,11 +1,16 @@
 package com.darrienglasser.refugevolunteerservice;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -14,6 +19,8 @@ import com.firebase.client.ValueEventListener;
 
 public class VolunteerPage extends AppCompatActivity {
     private static String TAG = "VolunteerPage";
+    private boolean receivedData;
+    private static final int REFRESH_ICON = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +32,35 @@ public class VolunteerPage extends AppCompatActivity {
 
         Firebase.setAndroidContext(this);
 
+        // Add back in when valid data is available
         Firebase myFirebaseRef = new Firebase("https://refuge.firebaseio.com/");
 
+        final HelpData userInfo = null;
 
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    HelpData userInfo = postSnapshot.getValue(HelpData.class);
+                    receivedData = true;
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.d(TAG, "Unable to read data");
+                receivedData = false;
+            }
+        });
 
         // TODO: Poll server to see if data has been received
-        if (false) {
-            foundReq.setVisibility(View.GONE);
-        } else {
+        if (receivedData) {
+            bindViews(userInfo);
+
             noReqView.setVisibility(View.GONE);
+        } else {
+            foundReq.setVisibility(View.GONE);
+
         }
     }
 
@@ -41,5 +68,48 @@ public class VolunteerPage extends AppCompatActivity {
     public void onBackPressed() {
         // Do nothing
         // We don't want to let the user go back to the parent activity
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case REFRESH_ICON:
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Refreshing content...",
+                        Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, REFRESH_ICON, Menu.NONE, "Refresh").setIcon(R.drawable.ic_refresh_icon);
+        return true;
+    }
+
+    private void bindViews(HelpData userInfo) {
+        TextView numText = (TextView) findViewById(R.id.numStatus);
+        TextView needText = (TextView) findViewById(R.id.needStatus);
+        TextView locText = (TextView) findViewById(R.id.locStatus);
+        TextView timeText = (TextView) findViewById(R.id.timeStatus);
+
+        String tmpNum = String.format(getResources().getString(
+                R.string.help_num_string), userInfo.getNumber());
+        String tmpNeed = String.format(getResources().getString(
+                R.string.help_num_string), userInfo.getNeed());
+        String tmpLoc = String.format(getResources().getString(
+                R.string.help_num_string), userInfo.getLocation());
+        String tmpTime = String.format(getResources().getString(
+                R.string.help_num_string), userInfo.getTimeStamp());
+
+        numText.setText(tmpNum);
+        needText.setText(tmpNeed);
+        locText.setText(tmpLoc);
+        timeText.setText(tmpTime);
+
     }
 }
